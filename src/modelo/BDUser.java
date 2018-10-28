9f567069e5e624db1efe0dfa270751c89f352f54
gomezportillo <pedroma.almagro@gmail.com>
2018-10-28 19:28:01 +0100

    added class provided by capel

---
+++ src/modelo/BDUser.java
@@ -0,0 +1,35 @@
+package modelo;
+
+import java.util.List;
+
+public class BDUser 
+{
+
+	public static List<User> listarUsuarios() 
+	{
+		return null;
+	}
+
+	public static boolean existeEmail(String email) 
+	{
+		return false;
+	}
+
+	public static User seleccionarUsuario(String email) 
+	{
+		return null;
+	}
+
+	public static void insertar(User user) 
+	{		
+	}
+
+	public static void actualizar(User user) 
+	{		
+	}
+
+	public static void eliminar(User user) 
+	{		
+	}
+
+}
